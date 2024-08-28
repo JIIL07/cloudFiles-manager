b32af5bfa84aa08287680dd3affca47f29b8297c
JIIL07 <romana9059@gmail.com>
2024-08-28 11:17:57 +0300

    Updated binary, updated script, adding config/ dir

--- deploy-linux.sh
+++ deploy-linux.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 TARGET_DIR="/opt/jcloud"
+CONFIG_DIR="/opt/jcloud/config"
 ARCHIVE_PATH="bin/jcloud.tar.gz"
 CONFIG_PATH="config/client.yaml"
 
 sudo mkdir -p $TARGET_DIR
+sudo mkdir -p $CONFIG_DIR
 
 sudo mv $ARCHIVE_PATH $TARGET_DIR/
-sudo mv $CONFIG_PATH $TARGET_DIR/
+sudo mv $CONFIG_PATH $CONFIG_DIR
 
 cd $TARGET_DIR
 sudo tar -xzvf jcloud.tar.gz
